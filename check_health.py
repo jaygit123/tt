979c14304d91422c0357899f07d43d7997add6df
jaygit123 <43414366+jaygit123@users.noreply.github.com>
2020-10-02 20:39:41 +0530

    Add files via upload

---
+++ check_health.py
@@ -0,0 +1,309 @@
+import os
+import ee
+import geemap
+import ipywidgets as widgets
+from IPython.display import display
+from IPython.display import clear_output
+from json import loads 
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+pd.options.mode.chained_assignment = None
+import plotly.express as px
+import plotly.graph_objects as go
+import warnings
+import random
+import datetime
+import dateutil.relativedelta
+warnings.filterwarnings('ignore')
+from tensorflow.python.util import deprecation
+from email_validator import validate_email, EmailNotValidError
+
+import send_mail
+import retrieve_obj
+import save_obj
+
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
+import plotly.io as pio
+pio.orca.config.use_xvfb = True
+
+#%matplotlib inline
+
+np.random.seed(1)
+
+# Retrieve the Object (csv file) containing list of emails from OCI Object storage
+BUCKET_NAME = "Bucket-for-crop-health-project"
+OBJECT_TO_RETRIEVE = 'check_health_file_obj.csv'
+status, filename_or_error = retrieve_obj.retrieve_object(BUCKET_NAME, OBJECT_TO_RETRIEVE)
+if status == False:
+    print('UNABLE TO RETRIEVE OBJECT...could not retrieve date from Object Storage')
+    send_mail.write_to_file('ERROR-OBJ_STORE_RETRV', filename_or_error)
+    raise Exception(filename_or_error)
+else:
+    print(status, filename_or_error)
+
+df1 = pd.read_csv(filename_or_error)
+location = list(df1['location'])
+mail = list(df1['mail id'])
+Map = geemap.Map()
+
+r1 = {"January":{"NDVI":{"min":0.61,"max":0.75},"EVI":{"min":1.233,"max":1.550}},
+      "February":{"NDVI":{"min":0.71,"max":0.85},"EVI":{"min":1.460,"max":1.777}},
+      "March":{"NDVI":{"min":0.725,"max":0.835},"EVI":{"min":1.494,"max":1.743}},
+      "April":{"NDVI":{"min":0.295,"max":0.415},"EVI":{"min":0.519,"max":0.791}},
+      "May":{"NDVI":{"min":0.101,"max":0.151},"EVI":{"min":0.079,"max":0.192}},
+      "June":{"NDVI":{"min":0.155,"max":0.215},"EVI":{"min":0.201,"max":0.337}},
+      "July":{"NDVI":{"min":0.3,"max":0.325},"EVI":{"min":0.530,"max":0.587}},
+      "August":{"NDVI":{"min":0.595,"max":0.715},"EVI":{"min":1.199,"max":1.471}},
+      "September":{"NDVI":{"min":0.565,"max":0.72},"EVI":{"min":1.131,"max":1.482}},
+      "October":{"NDVI":{"min":0.41,"max":0.55},"EVI":{"min":0.780,"max":1.097}},
+      "November":{"NDVI":{"min":0.101,"max":0.215},"EVI":{"min":0.079,"max":0.337}},
+      "December":{"NDVI":{"min":0.315,"max":0.487},"EVI":{"min":0.564,"max":0.954}}}
+
+r2 = {"January":{"NDVI":{"min":0.585,"max":0.708},"EVI":{"min":1.176,"max":1.455}},
+      "February":{"NDVI":{"min":0.685,"max":0.831},"EVI":{"min":1.403,"max":1.734}},
+      "March":{"NDVI":{"min":0.699,"max":0.825},"EVI":{"min":1.435,"max":1.720}},
+      "April":{"NDVI":{"min":0.285,"max":0.405},"EVI":{"min":0.496,"max":1.768}},
+      "May":{"NDVI":{"min":0.094,"max":0.145},"EVI":{"min":0.063,"max":0.179}},
+      "June":{"NDVI":{"min":0.145,"max":0.21},"EVI":{"min":0.0179,"max":0.326}},
+      "July":{"NDVI":{"min":0.285,"max":0.362},"EVI":{"min":0.496,"max":0.671}},
+      "August":{"NDVI":{"min":0.585,"max":0.705},"EVI":{"min":1.176,"max":1.448}},
+      "September":{"NDVI":{"min":0.555,"max":0.715},"EVI":{"min":1.108,"max":1.471}},
+      "October":{"NDVI":{"min":0.401,"max":0.503},"EVI":{"min":0.759,"max":0.990}},
+      "November":{"NDVI":{"min":0.121,"max":0.259},"EVI":{"min":0.124,"max":0.437}},
+      "December":{"NDVI":{"min":0.32,"max":0.475},"EVI":{"min":0.575,"max":0.927}}}
+
+r3 = {"January":{"NDVI":{"min":0.618,"max":0.746},"EVI":{"min":1.251,"max":1.541}},
+      "February":{"NDVI":{"min":0.718,"max":0.845},"EVI":{"min":1.478,"max":1.766}},
+      "March":{"NDVI":{"min":0.715,"max":0.827},"EVI":{"min":1.471,"max":1.725}},
+      "April":{"NDVI":{"min":0.299,"max":0.42},"EVI":{"min":0.528,"max":0.802}},
+      "May":{"NDVI":{"min":0.102,"max":0.155},"EVI":{"min":0.081,"max":0.201}},
+      "June":{"NDVI":{"min":0.149,"max":0.22},"EVI":{"min":0.188,"max":0.349}},
+      "July":{"NDVI":{"min":0.31,"max":0.321},"EVI":{"min":0.553,"max":0.578}},
+      "August":{"NDVI":{"min":0.601,"max":0.719},"EVI":{"min":1.213,"max":1.480}},
+      "September":{"NDVI":{"min":0.559,"max":0.729},"EVI":{"min":1.117,"max":1.503}},
+      "October":{"NDVI":{"min":0.415,"max":0.562},"EVI":{"min":0.791,"max":1.124}},
+      "November":{"NDVI":{"min":0.111,"max":0.249},"EVI":{"min":0.102,"max":0.415}},
+      "December":{"NDVI":{"min":0.331,"max":0.495},"EVI":{"min":0.600,"max":0.972}}}
+
+
+model = tf.keras.models.load_model('anamoly.h5')
+
+def regionReduce(img, props):
+  date = img[props[0]]
+  
+  
+  stat = img[props[1]].reduceRegion(
+    reducer = reReArgs['reducer'],
+    geometry = reReArgs['geometry'],
+    scale = reReArgs['scale'])
+
+  return(ee.Feature(None, stat).copyProperties(img[props[1]], img[props[1]].propertyNames()).set({'Date': date}))
+
+def getReReList(col, props):
+    d = []
+    for i in col:
+        r = regionReduce(i,props)
+        d.append(r)
+    da = ee.FeatureCollection(d)
+    dict = {}
+    dict =  da.filter(ee.Filter.notNull(props)).reduceColumns(reducer = ee.Reducer.toList().repeat(len(props)),selectors = props)
+
+    return (ee.List(dict.get('list')).getInfo())
+
+def create_seq(X,y,time_steps=1):
+    Xs,ys=[],[]
+    for i in range(len(X)-time_steps):
+        Xs.append(X.iloc[i:(i+time_steps)].values)
+        ys.append(y.iloc[i+time_steps])
+    return np.array(Xs), np.array(ys)
+
+def fill_values(df):
+    x = random.choice([r1,r2,r3])
+    for i in range(len(df.index)):
+      if pd.isna(df.at[i,'EVI']):
+        date = df.at[i,'date']
+        month = date.strftime("%B")
+        df.at[i,'EVI'] = random.uniform(x[month]['EVI']['min'],x[month]['EVI']['max'])
+      if pd.isna(df.at[i,'NDVI']):
+        date = df.at[i,'date']
+        month = date.strftime("%B")
+        df.at[i,'NDVI'] = random.uniform(x[month]['NDVI']['min'],x[month]['NDVI']['max'])
+    return df
+
+def create_df(NDVI,EVI):
+    NDVI = pd.DataFrame(NDVI)
+    NDVI = NDVI.transpose()
+    NDVI.columns = ['Date', 'NDVI']
+    NDVI['Date'] = pd.to_datetime(NDVI['Date'])
+    NDVI = NDVI.set_index('Date')
+    EVI = pd.DataFrame(EVI)
+    EVI = EVI.transpose()
+    EVI.columns = ['Date', 'EVI']
+    EVI['Date'] = pd.to_datetime(EVI['Date'])
+    EVI = EVI.set_index('Date')
+    df = pd.concat([NDVI,EVI],axis=1)
+    dates = pd.date_range(start=(datetime.datetime.strptime(from_date, "%Y-%m-%d") - dateutil.relativedelta.relativedelta(months=8)).strftime("%Y-%m-%d"),end=(to_date),freq='MS')
+    df = df.reindex(dates)
+    df = df.infer_objects()
+    df = df.reset_index()
+    df = df.rename(columns={"index":"date"})
+    df = fill_values(df)
+    return df
+
+def predict(df):
+    X,y = create_seq(df[['NDVI','EVI']],df.NDVI,8)
+    X_pred= model.predict(X)
+    mae_loss = np.abs(X_pred[:,0] - y)
+    
+    ndvi_thresholdn = 0.2
+    ndvi_thresholdl = 0.3
+    ndvi_thresholdu = 0.4
+    
+    score_df = pd.DataFrame(df[8:])
+
+    score_df['loss_NDVI'] = mae_loss
+    
+    score_df['threshold_NDVIn'] = ndvi_thresholdn
+    
+    score_df['threshold_NDVIl'] = ndvi_thresholdl
+    
+    score_df['threshold_NDVIu'] = ndvi_thresholdu
+    
+    score_df['anomaly_NDVIn'] = ((score_df.loss_NDVI > score_df.threshold_NDVIn) & (score_df.loss_NDVI < score_df.threshold_NDVIl) & (score_df.loss_NDVI < score_df.threshold_NDVIu))
+
+    score_df['anomaly_NDVIl'] = ((score_df.loss_NDVI > score_df.threshold_NDVIl) & (score_df.loss_NDVI < score_df.threshold_NDVIu))
+    
+    score_df['anomaly_NDVIu'] = score_df.loss_NDVI > score_df.threshold_NDVIu
+    
+    score_df['NDVI'] = df[8:].NDVI
+    
+    score_df['prediction'] = X_pred[:,0]
+    
+    anomalies_NDVIn = score_df[score_df.anomaly_NDVIn == True]
+    
+    anomalies_NDVIl = score_df[score_df.anomaly_NDVIl == True]
+    
+    anomalies_NDVIu = score_df[score_df.anomaly_NDVIu == True]
+    
+    
+    return anomalies_NDVIl,anomalies_NDVIu,anomalies_NDVIn,score_df
+
+def aplot(andvil,andviu,andvin,sdf,farm,name):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=andvin.date, y=andvin.NDVI,mode='markers',name='Mild anomaly',marker = dict(color = 'rgb(253, 229, 52)',size = 15)))
+    fig.add_trace(go.Scatter(x=andvil.date, y=andvil.NDVI,mode='markers',name='Medium anomaly',marker = dict(color = 'rgb(235, 153, 52)',size = 15)))
+    fig.add_trace(go.Scatter(x=andviu.date, y=andviu.NDVI,mode='markers',name='Severe anomaly',marker = dict(color = 'rgb(235, 52, 52)',size = 15)))
+    fig.add_trace(go.Scatter(x=sdf.date, y=sdf.NDVI,mode='lines',name='Health Graph',line = dict(color="#00fc41",width=4)))
+    fig.update_layout(title="Health Graph for "+farm,showlegend=True,font=dict(family="Courier New, monospace",size=18,color="RebeccaPurple"))
+    fig.write_image(name+".png")
+    return
+
+def cloudMaskL457(image):
+    qa = image.select('pixel_qa')
+    cloud = qa.bitwiseAnd(1 << 5).And(qa.bitwiseAnd(1 << 7)).Or(qa.bitwiseAnd(1 << 3))
+
+    mask2 = image.mask().reduce(ee.Reducer.min())
+    return image.updateMask(cloud.Not()).updateMask(mask2)
+
+def EVI(image):
+    return image.expression(
+        '(2.5*(NIR-R))/(NIR+(2.4*R)+1)', 
+        {
+        'NIR': image.select('B4'),
+        'R': image.select('B3')
+        })
+
+def ProcessImg(region,from_date,to_date,clouds_percentage):
+    
+    NUM_OF_MONTHS = ee.Date(to_date).difference(from_date, 'month')
+    
+    region_to_clip = ee.FeatureCollection(region)
+
+    NUM_OF_MONTHS_TO_DISPLAY = NUM_OF_MONTHS.round()
+
+    Filtered_Collection = Landsat7_tier1_dataset.filterBounds(region_to_clip)
+
+    filteredRegion_Date_month_to_display = Filtered_Collection.filterDate(from_date, to_date)
+    
+    GLOBAL_NDVI = []
+    GLOBAL_EVI = []
+    delta = 0
+    j = 0
+    for i in range(NUM_OF_MONTHS_TO_DISPLAY.getInfo()):
+        from_date_next = ee.Date(from_date).advance(delta, 'month')
+        to_date_next = from_date_next.advance(30, 'day')
+        filteredRegion_Date_month = Filtered_Collection.filterDate(from_date_next, to_date_next)
+        filteredRegion_Date_month_unmastCloud = filteredRegion_Date_month.map(cloudMaskL457)
+
+        numImg = filteredRegion_Date_month_unmastCloud.size().getInfo()
+
+        if(numImg < 1):
+            delta+=1
+            continue
+        
+        medianRGB = filteredRegion_Date_month_unmastCloud.median()
+        medianNDVI = medianRGB.normalizedDifference(['B4','B3'])
+        NDVI_VALUE_DICT = {"Date": from_date_next.format('dd MMM YYYY').getInfo(), "NDVI": ee.Image(medianNDVI).select([0], ['NDVI'])}
+        GLOBAL_NDVI.append(NDVI_VALUE_DICT)
+
+        medianEV = filteredRegion_Date_month_unmastCloud.map(EVI)
+        medianEVI = medianEV.median()
+        EVI_VALUE_DICT = {"Date": from_date_next.format('dd MMM YYYY').getInfo(), "EVI": ee.Image(medianEVI).select([0], ['EVI'])}
+        GLOBAL_EVI.append(EVI_VALUE_DICT)
+
+        delta+=1
+        j+=1
+        
+    return GLOBAL_NDVI,GLOBAL_EVI
+
+Landsat7_tier1_dataset = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR")
+
+reReArgs = {
+'reducer': ee.Reducer.mean(),
+'geometry': ee.Geometry.Point([0,0]),
+'scale': 200}
+
+x = datetime.datetime.now()
+date = x.strftime("%Y-%m-%d")
+d = datetime.datetime.strptime(date, "%Y-%m-%d")
+d2 = d - dateutil.relativedelta.relativedelta(months=8)
+to_date = date
+from_date = d2.strftime("%Y-%m-%d")
+clouds_percentage = 100
+cnt = 1
+for i in range(len(df1.index)):
+    id = mail[i]
+    loc = location[i]
+    
+    region1 = ee.Geometry.Polygon(loads(loc))
+
+    bigger_region = region1.buffer(0.1)
+            
+    reReArgs['geometry'] = region1
+    NDVI,EVI1 = ProcessImg(bigger_region,from_date,to_date,clouds_percentage)
+    ndvi = getReReList(NDVI, ['Date', 'NDVI'])
+    evi = getReReList(EVI1, ['Date', 'EVI'])
+    df = create_df(ndvi,evi)
+    andvil1,andviu1,andvin1,sdf1 = predict(df)
+    aplot(andvil1, andviu1, andvin1, sdf1, 'Farm ' + cnt, id + '_' + cnt)
+    cnt = cnt + 1
+
+status = send_mail.send_email(mail)
+print("results -----------")
+if status == False:
+    print('UNABLE TO SEND EMAILs...could not retrieve SMTP server details')
+    send_mail.write_to_file('ERROR-EMAIL', 'UNABLE TO SEND EMAILs...could not retrieve SMTP server details')
+else:
+    print(status)
+    #send_mail.write_to_file('STATUS-EMAIL', str(status))
+
+    OBJECT_TO_SAVE = 'check_health_status_obj_'+str(datetime.date.today())+'.txt'
+    status, filename_or_error = save_obj.put_object_to_storage(BUCKET_NAME, OBJECT_TO_SAVE, str(status))
+    if status == False:
+        print('UNABLE TO SAVE RESULTS OBJECT...could not save results data to Object Storage')
+        send_mail.write_to_file('ERROR-OBJ_STORE_SAVE', filename_or_error)
+        raise Exception(filename_or_error)
+    else:
+        print(status, filename_or_error)
---
+++ send_mail.py
@@ -0,0 +1,151 @@
+#
+# oci-email-send-python version 1.0.
+#
+# Copyright (c) 2020 Oracle, Inc.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+import io, os, json, smtplib, email.utils, random, glob
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders 
+import datetime as DT
+from cryptography.fernet import Fernet 
+
+
+def write_to_file(prefix, content):
+    n = str(random.random())
+    today_date = str(DT.date.today())
+    #print(">>>> " + n)
+    #print(">>>> " + today_date)
+    f = open(prefix + "-" + today_date + "-" + n + ".txt", "a")
+    f.write(today_date + ": " + content)
+    f.close()
+
+def send_email(mail_contents):
+    try:
+        smtp_username, smtp_password = get_cred()
+    except Exception as ex:
+        print("ERROR in getting SMTP details: ", ex, flush=True)
+        write_to_file("ERROR-SMTP", "ERROR in getting SMTP details: " + str(ex))
+        return False
+
+    smtp_host = "smtp.us-ashburn-1.oraclecloud.com"
+    smtp_port = 587
+    sender_email = "Connect@DeepVisionTech.AI"
+    sender_name = "Crop Health (DeepVisionTech.AI)"
+    recipient = subject = body = ""
+    STATUS = []
+
+    for mail_con in mail_contents:
+        success_flag1 = success_flag2 = success_flag3 = True
+        #recipient = mail_con['recipient']
+        recipient = mail_con
+
+        try: 
+            fromDate = DT.date.today()
+            toDate = fromDate - DT.timedelta(days=7)
+            subject = "Crop Health for the week " + str(toDate) + " to " + str(fromDate)
+            #print("subject: " + subject) 
+            body = "Hello there, \n\nWe hope you are doing well.\nPlease find attached your weekly Crop Health for the week " \
+                                        + str(toDate) + " to " + str(fromDate) + "."\
+                                        + "\n\nNOTE: The attached graph has health status for the past 8 months." \
+                                        + "\n\nThanks & Regards,\nTeam DeepVisionTech.AI" \
+                                        + "\n\nVisit us: https://DeepVisionTech.AI" \
+            #print("Body: " + body) 
+        except Exception as ex:
+            print("ERROR in generating email headers: ", ex, flush=True)
+            #raise
+            success_flag1 = False
+            STATUS.append({recipient: "FAILED"})
+            continue
+
+        try:
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = subject
+            msg['From'] = email.utils.formataddr((sender_name, sender_email))
+            msg['To'] = recipient
+            msg.attach(MIMEText(body, 'plain'))
+
+            #filename = recipient + ".png"
+            files_for_email = glob.glob(recipient + '*.png')
+            print(">>> Got "+str(len(files_for_email))+" image files for email " + recipient)
+            for img_file in files_for_email:
+                print("FILE: " + os.path.basename(img_file))
+                attachment = open("./" + os.path.basename(img_file), "rb") 
+                p = MIMEBase('application', 'octet-stream') 
+                p.set_payload((attachment).read()) 
+                encoders.encode_base64(p) 
+                p.add_header('Content-Disposition', "attachment; filename=" + os.path.basename(img_file)) 
+                msg.attach(p) 
+        except Exception as ex:
+            print("ERROR in generating email body: ", ex, flush=True)
+            #raise
+            success_flag2 = False
+            if success_flag1 == True:
+                STATUS.append({recipient: "FAILED"})
+            continue
+
+        try: 
+            server = smtplib.SMTP(smtp_host, smtp_port)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, recipient, msg.as_string())
+            server.close()
+        except Exception as ex:
+            print("ERROR in sending email: ", ex, flush=True)
+            #raise
+            success_flag3 = False
+            if (success_flag1 == True) and (success_flag2 == True):
+                STATUS.append({recipient: "FAILED"})
+            continue
+        else:
+            print ("INFO: Email successfully sent!", flush=True)
+
+        STATUS.append({recipient: "SUCCESS"})
+
+    return STATUS
+
+
+def get_cred():
+    cred_filename = 'CredFile.ini'
+    key_file = 'key.key'
+
+    key = '' 
+
+    with open('key.key','r') as key_in: 
+        key = key_in.read().encode() 
+
+    #If you want the Cred file to be of one 
+    # time use uncomment the below line 
+    #os.remove(key_file) 
+
+    f = Fernet(key) 
+    with open(cred_filename,'r') as cred_in: 
+        lines = cred_in.readlines() 
+        config = {} 
+        for line in lines: 
+            tuples = line.rstrip('\n').split('=',1) 
+            if tuples[0] in ('Username','Password'): 
+                config[tuples[0]] = tuples[1] 
+
+        username = (config['Username'])
+        passwd = f.decrypt(config['Password'].encode()).decode() 
+        #print("Username:", username)
+        #print("Password:", passwd) 
+
+    return username, passwd
+
+
+if __name__ == '__main__':
+    mail_contents = ["jay.test.test1@gmail.com"] # NOTE: update with email ids to test
+    status = send_email(mail_contents)
+    if status == False:
+        print('UNABLE TO SEND EMAILs...could not retrieve SMTP server details')
+        write_to_file('ERROR-EMAIL', 'UNABLE TO SEND EMAILs...could not retrieve SMTP server details')
+    else:
+        print(status)
+        write_to_file('STATUS-EMAIL', str(status))
